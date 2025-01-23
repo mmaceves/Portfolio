45eec14caca709775f431a52b6e6cf9728b55698
Ace <matt.aceves@yahoo.com>
2025-01-22 21:06:26 -0700

    fixed app and main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Home from './components/Home';
+import { Outlet } from 'react-router-dom';
 
 function App() {
     return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </Router>
+        <>
+        <Outlet />
+        </>
     );
 }
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import App from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Projects from './components/Projects';
+import ErrorPage from './pages/ErrorPage';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import About from './pages/About';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+    
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: 'projects',
+        element: <Projects />
+    },
+    {
+        path: 'contact',
+        element: <Contact />
+    },
+    {
+        path: 'about',
+        element: <About />
+    }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
