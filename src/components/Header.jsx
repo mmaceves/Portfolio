171961564a70506c850c48d02d924ff4131da8ae
Ace <matt.aceves@yahoo.com>
2025-01-22 21:05:38 -0700

    fixed components files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 // import { Link } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
 import  NavBar from './NavBar';
 
 function Header() {
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,24 +5,24 @@ function Projects() {
     return (
         <div>
             <Header />
-            <div class="page-section" id="work">
+            <div className="page-section" id="work">
             <h2>Projects</h2>
                 <div>
-                <a href="#" class="flex-item led-wall">
-                    <h2>Employee Tracker</h2>
-                </a>
-                
-                <a href="#" class="flex-item run-buddy">
-                    <h2>Vehicle Builder</h2>
-                </a>
+                    <a href="https://johnbrunson.github.io/the-gamers-journal/" className="flex-item">
+                        <h2>Gamers Journal</h2>
+                    </a>
 
-                <a href="#" class="flex-item pastel-puzzles">
-                    <h2>Readme Generator</h2>
-                </a>
+                    <a href="https://github.com/mmaceves/Employee-Tracker" className="flex-item">
+                        <h2>Employee Tracker</h2>
+                    </a>
 
-                <a href="#" class="flex-item pastel-puzzles">
-                    <h2>Gamers Journal</h2>
-                </a>
+                    <a href="https://github.com/mmaceves/vehicle-builder" className="flex-item">
+                        <h2>Vehicle Builder</h2>
+                    </a>
+            
+                     <a href="https://github.com/mmaceves/readme-generator" className="flex-item">
+                        <h2>Readme Generator</h2>
+                    </a>
 
                 </div>
             </div>
